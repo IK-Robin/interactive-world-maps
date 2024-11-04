ed5cdd855a6e8efc8dfeda6fccb3a4a7660c47ff
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-11-04 13:09:33 +0600

    complite backend tooltip style

--- views/tooltip.php
+++ views/tooltip.php
@@ -3,10 +3,11 @@
 // tooltip page 
 ?>
 
+<div id="ikr_map_tooltip" class="ikr_map_tooltip"></div>
 <div class="detail" id="detail">
         <div style="position: relative">
           <div id="detail_img" class="ikr_detail_img">
-            <img src="" alt="" id="map_img" />
+            <img src="<?php echo plugins_url('../assets/images/worldmap.svg',__FILE__); ?>" alt="" id="map_img" />
           </div>
           <div class="map_details">
             <p id="plotId" class="map_title"></p>
