8990acda096edef1d410c1ee23ed11a43cc90448
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-11-06 09:52:41 +0600

    add color to the map

--- assets/js/ikrwmap-fontend-script.js
+++ assets/js/ikrwmap-fontend-script.js
@@ -2,12 +2,13 @@ const ikrgooMap = document.querySelector(".svg_img_obj");
 const ikr_map_tooltip = document.getElementById("ikr_map_tooltip");
 const ikrwmap_details =document.getElementById('ikrwmap_details');
 const ikrwmap_output  = document.getElementById("ikrwmap_output");
+
+
 document.addEventListener("DOMContentLoaded", (event) => {
   // Create the SVG element and set attributes
   const ikrwmap_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   ikrwmap_svg.setAttribute("viewBox", "0 0 1000 640");
-  ikrwmap_svg.setAttribute("width", "100%");
-  ikrwmap_svg.setAttribute("height", "auto");
+  
   ikrwmap_svg.setAttribute("id", "svg4");
 
 
@@ -18,58 +19,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   // Append the SVG to the output container
   document.getElementById("ikrwmap_output").appendChild(ikrwmap_svg);
-});
+  const svg4 = document.getElementById('svg4');
+
+  let items = svg4.querySelectorAll("rect,path", "circle", "polygon");
 
 
 
 
-
-
-
-
-ikrgooMap.addEventListener("load", (irkcontent) => {
-  // get the svg
-  const ikrsvgDocc = ikrgooMap.contentDocument;
-  const ikrsvg = ikrsvgDocc.querySelector("svg");
-
-  console.log(ikrsvg);
-  let items = ikrsvg.querySelectorAll("rect,path", "circle", "polygon");
-
-  items.forEach((ev, ind) => {
-    let ids = ev.id;
-    let id = {
-      id: ids,
-    };
+    items.forEach((svg_path) => {
+    svg_path.addEventListener("click", (ev) => {
+      console.log(ev.offsetX)
+      const ct_dataset = ev.target.dataset;
+      if (ct_dataset.img || ct_dataset.link|| ct_dataset.title||ct_dataset.desc ) {
+        ikrwmap_details.style.display = "block";
+        ikrwmap_details.style.left = ev.pageX + "px";
+        ikrwmap_details.style.top = ev.pageY + "px";
+      }
+    });
   });
 
-  // select the svg path
-  // console.log(tab)
 
-  // map the item to  the dom and  add event listener
 
-  // items.forEach((svg_path) => {
-  //   svg_path.addEventListener("click", (ev) => {
-  //     console.log(ev.offsetX)
-  //     const ct_dataset = ev.target.dataset;
-  //     if (ct_dataset.img || ct_dataset.link|| ct_dataset.title||ct_dataset.desc ) {
-  //       ikrwmap_details.style.display = "block";
-  //       ikrwmap_details.style.left = ev.pageX + "px";
-  //       ikrwmap_details.style.top = ev.pageY + "px";
-  //     }
-  //   });
-  // });
-console.log(ikrwmap_output)
-  ikrwmap_output.addEventListener('click',(ev) =>{
-    console.log(ev.offsetX)
-    const ct_dataset = ev.target.dataset;
-    if (ct_dataset.img || ct_dataset.link|| ct_dataset.title||ct_dataset.desc ) {
-      ikrwmap_details.style.display = "block";
-      ikrwmap_details.style.left = ev.pageX + "px";
-      ikrwmap_details.style.top = ev.pageY + "px";
-    }
-  });
 
-  async function ikrwmap_retrive_data_from_db() {
+ 
+   async function ikrwmap_retrive_data_from_db() {
     try {
       // fetch the data from the db
       const response = await world_map_fetchAjaxRequest(
@@ -77,7 +50,7 @@ console.log(ikrwmap_output)
         ikrwmap_get_url.ajax_url
       );
 
-      console.log(response);
+      // console.log(response);
       // check the  response status code
 
       if (response.length == 0) {
@@ -87,7 +60,7 @@ console.log(ikrwmap_output)
         items.forEach((mapId) => {
           response.forEach((data) => {
             if (mapId.id == data.map_id) {
-              const setColor = ikrsvg.querySelector(`#${mapId.id}`);
+              const setColor = svg4.querySelector(`#${mapId.id}`);
 
               setColor.style.fill = `${data.fill_color}`;
               setColor.setAttribute("data-img", data.map_img);
@@ -101,7 +74,91 @@ console.log(ikrwmap_output)
   }
 
   ikrwmap_retrive_data_from_db();
+
 });
+
+
+
+
+
+
+
+
+// ikrgooMap.addEventListener("load", (irkcontent) => {
+//   // get the svg
+//   const ikrsvgDocc = ikrgooMap.contentDocument;
+//   const ikrsvg = ikrsvgDocc.querySelector("svg");
+
+//   // console.log(ikrsvg);
+//   let items = ikrsvg.querySelectorAll("rect,path", "circle", "polygon");
+
+//   items.forEach((ev, ind) => {
+//     let ids = ev.id;
+//     let id = {
+//       id: ids,
+//     };
+//   });
+
+//   // select the svg path
+//   // console.log(tab)
+
+//   // map the item to  the dom and  add event listener
+
+//   // items.forEach((svg_path) => {
+//   //   svg_path.addEventListener("click", (ev) => {
+//   //     console.log(ev.offsetX)
+//   //     const ct_dataset = ev.target.dataset;
+//   //     if (ct_dataset.img || ct_dataset.link|| ct_dataset.title||ct_dataset.desc ) {
+//   //       ikrwmap_details.style.display = "block";
+//   //       ikrwmap_details.style.left = ev.pageX + "px";
+//   //       ikrwmap_details.style.top = ev.pageY + "px";
+//   //     }
+//   //   });
+//   // });
+
+//   ikrwmap_output.addEventListener('click',(ev) =>{
+  
+//     const ct_dataset = ev.target.dataset;
+//     if (ct_dataset.img || ct_dataset.link|| ct_dataset.title||ct_dataset.desc ) {
+//       ikrwmap_details.style.display = "block";
+//       ikrwmap_details.style.left = ev.pageX + "px";
+//       ikrwmap_details.style.top = ev.pageY + "px";
+//     }
+//   });
+
+//   async function ikrwmap_retrive_data_from_db() {
+//     try {
+//       // fetch the data from the db
+//       const response = await world_map_fetchAjaxRequest(
+//         ikrwmap_get_url.featchdata,
+//         ikrwmap_get_url.ajax_url
+//       );
+
+//       // console.log(response);
+//       // check the  response status code
+
+//       if (response.length == 0) {
+//         return;
+//       } else {
+//         // set the color of  the map based on the data
+//         items.forEach((mapId) => {
+//           response.forEach((data) => {
+//             if (mapId.id == data.map_id) {
+//               const setColor = ikrsvg.querySelector(`#${mapId.id}`);
+
+//               setColor.style.fill = `${data.fill_color}`;
+//               setColor.setAttribute("data-img", data.map_img);
+//             }
+//           });
+//         });
+//       }
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+
+//   ikrwmap_retrive_data_from_db();
+// });
 
 function world_map_fetchAjaxRequest(actions, ajaxurl) {
   return new Promise((resolve, reject) => {
