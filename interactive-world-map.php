59f42a56d641867947751b99f5155daa029d0f30
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-12-03 13:42:37 +0600

    rename the plugin name and menu title

--- interactive-world-map.php
+++ interactive-world-map.php
@@ -1,6 +1,6 @@
 <?php /**
  
- * Plugin Name:       interactive world map plugin     
+ * Plugin Name:       Interactive World Maps
  * Plugin URI:        https://wordpress.org/plugins/search/interactive-geo-maps-wp/
  * Description:       INteractive world map plugin for WordPress. 
  * Version:           1.0.0
@@ -22,11 +22,11 @@ if ( ! defined( 'IKRWMAP_ROBIN_DIR_PATH_WORLD' ) ) {
 include_once IKRWMAP_ROBIN_DIR_PATH_WORLD . './functions/functions.php';
 function ikrwmap_add_menu_page()
 {
-   add_menu_page('interactive-world-map-robin', 'interactive-world-map-robin', 'manage_options', 'interactive-world-map-robin', 'ikrwmap_ikrwmap_add_admin_menu_page', '', 101);
-   add_submenu_page('interactive-world-map-robin', 'Dashbord', 'interactive world map', 'manage_options', 'interactive-world-map-robin', '', 'ikrwmap_ikrwmap_add_admin_menu_page');
+   add_menu_page('interactive-world-maps', 'world-maps', 'manage_options', 'interactive-world-maps', 'ikrwmap_ikrwmap_add_admin_menu_page', '', 101);
+   add_submenu_page('interactive-world-maps', 'Dashboard', 'Dashboard', 'manage_options', 'interactive-world-maps', '', 'ikrwmap_ikrwmap_add_admin_menu_page');
 
    // add show data submenu 
-   add_submenu_page('interactive-world-map-robin', 'map-data', 'map-data', 'manage_options', 'interactive-world-map-robin', 'ikrwmap_add_show_data', 101);
+   add_submenu_page('interactive-world-maps', 'Map Shortcode', 'Map Shortcode', 'manage_options', 'interactive-world-maps-shortcode', 'ikrwmap_add_show_data', 101);
 
 
 }
