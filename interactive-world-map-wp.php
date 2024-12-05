11d823272779932bb02811fee97978f27b44c2f0
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-12-05 13:55:09 +0600

    rename the file

--- interactive-world-map-wp.php
+++ interactive-world-map-wp.php
@@ -1,14 +1,14 @@
 <?php /**
  
- * Plugin Name:       Interactive World Maps
- * Plugin URI:        https://wordpress.org/plugins/search/interactive-geo-maps-wp/
+ * Plugin Name:       Interactive World Maps WP
+ * Plugin URI:        https://wordpress.org/plugins/interactive-world-maps-wp/
  * Description:       INteractive world map plugin for WordPress. 
  * Version:           1.0.0
  * Author:            IK Robin
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.robin.org/licenses/gpl-2.0.txt
- * Text Domain:       interactiveworld-map
+ * Text Domain:       interactive-world-maps
  * Domain Path:       /languages
  */
 
@@ -22,11 +22,11 @@ if ( ! defined( 'IKRWMAP_ROBIN_DIR_PATH_WORLD' ) ) {
 include_once IKRWMAP_ROBIN_DIR_PATH_WORLD . './functions/functions.php';
 function ikrwmap_add_menu_page()
 {
-   add_menu_page('interactive-world-maps', 'world-maps', 'manage_options', 'interactive-world-maps', 'ikrwmap_ikrwmap_add_admin_menu_page', '', 101);
-   add_submenu_page('interactive-world-maps', 'Dashboard', 'Dashboard', 'manage_options', 'interactive-world-maps', '', 'ikrwmap_ikrwmap_add_admin_menu_page');
+   add_menu_page('interactive-world-maps-wp', 'world-maps', 'manage_options', 'interactive-world-maps-wp', 'ikrwmap_ikrwmap_add_admin_menu_page', '', 101);
+   add_submenu_page('interactive-world-maps-wp', 'Dashboard', 'Dashboard', 'manage_options', 'interactive-world-maps-wp', '', 'ikrwmap_ikrwmap_add_admin_menu_page');
 
    // add show data submenu 
-   add_submenu_page('interactive-world-maps', 'Map Shortcode', 'Map Shortcode', 'manage_options', 'interactive-world-maps-shortcode', 'ikrwmap_add_show_data', 101);
+   add_submenu_page('interactive-world-maps-wp', 'Map Shortcode', 'Map Shortcode', 'manage_options', 'interactive-world-maps-wp-shortcode', 'ikrwmap_add_show_data', 101);
 
 
 }
@@ -41,11 +41,12 @@ function ikrwmap_add_show_data()
 {
 
    ?>
-   <h1> show from data</h1>
-  
+   <h1> Map Shortcode</h1>
+  <br>
+  <h3>Copy the Shortcode and past it in your page or post</h3>
+  <br>
+<h1> <strong>Shortcode = </strong> [ikr_world_map]</h1>
 
-
-<?php  include_once IKRWMAP_ROBIN_DIR_PATH_WORLD . '/views/show_data.php' ?>
 
 
    <?php
